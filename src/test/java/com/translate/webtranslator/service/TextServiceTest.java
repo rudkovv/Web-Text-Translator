040c6f6b1d83117f84f138208e8be9ce5ea4ce81
Lenovo <Lenovo@DESKTOP-IKQMRSL>
2024-04-16 10:56:18 +0300

    test update 3

--- src/test/java/com/translate/webtranslator/service/TextServiceTest.java
+++ src/test/java/com/translate/webtranslator/service/TextServiceTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
